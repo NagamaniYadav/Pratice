4847ccc82f50c2d6b42679b046b1a0c3d1045809
NagamaniYadav <49432875+NagamaniYadav@users.noreply.github.com>
2019-04-28 19:15:50 +0530

    dog1

--- Dog's age.py
+++ Dog's age.py
@@ -2,6 +2,7 @@ a=int(input("Input a dog's age in human years : "))
 if(a>0):
     if(a<=2):
         age=a*10.5
+        print("Age of the Dog")
         print("The dog's age in dog's years is ",age)
     if((a-2)>=2):
         age=((a-2)*4)+21
